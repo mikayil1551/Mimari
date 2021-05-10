8114a87b90b663f0ea0b3f9e02806d430e0bbc49
Sadigzade Mikayil <75858136+mikayil1551@users.noreply.github.com>
2021-05-10 22:04:33 +0300

    Add files via upload

---
+++ Mimari/6763Mimari/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6763Mimari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Urunler2Form uf = new Urunler2Form();
+        private void urunlerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (uf.IsDisposed)
+                uf = new Urunler2Form();
+            uf.MdiParent = this;
+            uf.Show();
+
+        }
+        KategorilerForm kf = new KategorilerForm();
+        private void kategorilerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (kf.IsDisposed)
+                kf = new KategorilerForm();
+            kf.MdiParent = this;
+            kf.Show();
+        }
+    }
+
+   
+}
---
+++ Mimari/6763Mimari/KategorilerForm.cs
@@ -0,0 +1,56 @@
+﻿using Mimari.Entity;
+using Mimari.ORM;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6763Mimari
+{
+    public partial class KategorilerForm : Form
+    {
+        public KategorilerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void KategorilerForm_Load(object sender, EventArgs e)
+        {
+            Listele();
+
+        }
+        KategorilerORM orm = new KategorilerORM();
+        private void Listele()
+        {
+            orm.Sil(14);
+            dataGridView1.DataSource = orm.Listele();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            Kategoriler kg = new Kategoriler();
+            kg.KategoriAdi = txtAdi.Text;
+            kg.Tanimi = txtTanimi.Text;
+            kg.Resim = new byte[0];
+            orm.Ekle(kg);
+            Listele();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Kategoriler kg = new Kategoriler();
+            orm.Guncelle(kg);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Parameters.AddWithValue("@KategoriID", dataGridView1.CurrentRow.Cells["KategoriID"].Value);
+            cmd.Parameters.AddWithValue("@KategoriAdi", dataGridView1.CurrentRow.Cells["KategoriAdi"].Value);
+            cmd.Parameters.AddWithValue("@Tanimi", dataGridView1.CurrentRow.Cells["Tanimi"].Value);
+            cmd.Parameters.AddWithValue("@Resim", dataGridView1.CurrentRow.Cells["Resim"].Value);
+        }
+    }
+}
